47d0c4dc62f7e90a7b766a32d5990f05ef4d2250
Luis Carlos Bicho <Luis.bicho@c-intact.local>
2025-10-01 09:17:10 +0200

    Project created

---
+++ src/main/java/com/luis/mini_dms/MiniDmsApplication.java
@@ -0,0 +1,13 @@
+package com.luis.mini_dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/mini_dms/MiniDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.mini_dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
